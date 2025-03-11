7a16dcad95d8c8176fb27428f310f51c9bd309ff
Pierre <pierreastor25@gmail.com>
2025-03-11 22:45:03 +0100

    Ajout du choix du type de blague

--- projet-blagues/main.py
+++ projet-blagues/main.py
@@ -1,7 +1,9 @@
-import pyjokes 
+import pyjokes
 
-def tell_joke():
-    print(pyjokes.get_joke())
+def tell_joke(lang="en", joke_type="neutral"):
+    print(pyjokes.get_joke(language=lang, category=joke_type))
 
 if __name__ == "__main__":
-    tell_joke()
+    lang = input("Choisissez une langue (en, de, es, it, gl): ")
+    joke_type = input("Choisissez une catégorie de blague (neutral, chuck, all): ")
+    tell_joke(lang, joke_type)
